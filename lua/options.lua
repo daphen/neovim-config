6838946a1c2a6022a43603352e2139fc1fbf2db8
David Karlsson <david.karlsson@utvecklarbolaget.se>
2024-04-06 19:05:43 +0200

    some changes and additions

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,3 @@
-
 local opt = vim.opt
 
 -- line number
@@ -12,13 +11,13 @@ opt.expandtab = true
 opt.autoindent = true
 
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- Sync clipboard between OS and Neovim.
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,12 @@
 return {
-	"tpope/vim-fugitive",
+	{ "tpope/vim-fugitive" },
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-		config = true,
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Preview git hunk" })
+		end,
 	},
 }
